a5bbcaefdddb567757c346dc854a1f0609892ca5
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-14 11:50:18 +0300

    created tasks files

---
+++ myVariables.js
@@ -0,0 +1,15 @@
+let language = "JavaScript";
+
+let score = 10;
+
+console.log(language + score);
+
+let length = 4;
+let width = 5;
+
+console.log("the area of the rectangle is" + " " + length * width);
+
+let num1 = 8;
+let num2 = 3;
+
+console.log(num1 % num2);
