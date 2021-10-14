3110bceeccb92ba9b8c338cecccb4e1d7d0140c1
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-14 13:34:14 +0300

    created Main

---
+++ main.js
@@ -0,0 +1,11 @@
+let str = "heLLo wOrld";
+
+let control = "upper";
+
+if (control === "upper") {
+  ///convert str to upper case
+  console.log((str = str.toLocaleUpperCase()));
+} else {
+  ///convert str to lower case
+  console.log((str = str.toLocaleLowerCase()));
+}
