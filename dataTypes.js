369692ab7a618cda7100b3c6d7abcacfe9515e9c
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-14 13:08:47 +0300

    updated second task

---
+++ dataTypes.js
@@ -0,0 +1,13 @@
+let x ;
+x = -2.3;
+
+let y = String;
+y = "hello";
+
+let z = Array;
+z = [2, 4, 6];
+
+let a = Object;
+a = { firstWord: "New", secondWord: "World!" };
+
+console.log(typeof x, y, z, a);
