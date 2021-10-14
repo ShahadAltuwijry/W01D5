dd42b9ea0f989385e9185aa5a1a1377ffdd72572
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-10-14 14:03:29 +0300

    updated main

--- main.js
+++ main.js
@@ -6,6 +6,6 @@ if (control === "upper") {
   ///convert str to upper case
   console.log((str = str.toLocaleUpperCase()));
 } else {
-  ///convert str to lower case
+  ///convert str to lower case 
   console.log((str = str.toLocaleLowerCase()));
 }
